636acb2245b48dfaccbd049f4033215724b9bc7b
rebeccabartels <rebeccakbartels@icloud.com>
2016-09-03 21:44:57 -0400

    fixed the error that type viewcontroller doesn't conform to protocol uipickerdelegate ect. by adding the functions after didRecieveMemoryWarning

--- PickinFruit/ViewController.swift
+++ PickinFruit/ViewController.swift
@@ -8,21 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIPickerViewDelegate, UIPickerView, UIPickerViewDataSource {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
+    @IBOutlet weak var picker: UIPickerView!
     
+    var pickerData: [String] = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+         self.spinButton.accessibilityLabel = Constants.SPIN_BUTTON
+        
+        // Connect data:
+        self.picker.delegate = self
+        self.picker.dataSource = self
+        
+        // Input data into the Array:
+        pickerData = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6"]
+    }
     @IBOutlet weak var spinButton: UIButton!
     @IBOutlet weak var resultLabel: UILabel!
     
     var fruitsArray = ["🍎", "🍊", "🍌", "🍐", "🍇", "🍉", "🍓", "🍒", "🍍"]
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        self.spinButton.accessibilityLabel = Constants.SPIN_BUTTON
-        
-    }
-    
+
     func fruitpickerString (string: String) {
         
         
@@ -31,6 +40,8 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerView, UIPi
     
     func pickerView(_ pickerView: UIPickerView,
                       numberOfRowsInComponent component: Int) -> Int {
+        
+        return 1
         
     }
     
@@ -60,6 +71,27 @@ extension ViewController {
         self.spinButton.layer.borderColor = UIColor.whiteColor().CGColor
         self.spinButton.layer.borderWidth = 4.0
         self.spinButton.clipsToBounds = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    // The number of columns of data
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // The number of rows of data
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    // The data to return for the row and component (column) that's being passed in
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
     }
     
 }
